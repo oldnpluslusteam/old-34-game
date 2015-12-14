5430fe4a41d26ebed12d93a071dbe7378d154852
A.Bond <alexey.bond.94.55@gmail.com>
2015-12-14 23:55:12 +0600

    fix

--- ui/button.py
+++ ui/button.py
@@ -1,19 +1,16 @@
 __author__ = 'moardm'
 
-from fwk.ui.layers.guitextitem import GUITextItem
+from fwk.ui.layers.guiItem import GUIItemLayer
 from fwk.util.graphics import LoadTexture
 from fwk.ui.layers.texture9TileItem import _9Tiles
-from fwk.util.rect import Rect
+from fwk.util.all import *
 
-class Button(GUITextItem):
+class Button(GUIItemLayer):
 
 	def init(self, onclick, img, layout, *args, **kwargs):
 		self.on('ui:click', onclick)
-		self.img = _9Tiles(LoadTexture(img), Rect(left=0, bottom=0, width=layout['width'], height=layout['height']))
-		self.on_layout_updated()
+		self.img = LoadTexture(img)
 
 	def draw(self):
-		self.img.draw(self.rect)
-
-	def on_layout_updated(self):
-		self._inrect = None
+		BlitTextureToRect(self.img, self.rect)
+		DrawWireframeRect(self.rect)
